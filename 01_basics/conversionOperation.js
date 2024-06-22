96b36e194e12e6fba69a7147cca07dbd7fb689c5
Kumar Ayush <108970673+KumarAyushh@users.noreply.github.com>
2024-06-22 09:10:25 +0000

    Operations part in JS

--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -19,5 +19,27 @@ let booleanIsLoggedIn = Boolean(isLogggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+// ************** Operations **************
+// console.log(2+2)
+// console.log(2-2);
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+console.log(str3);
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+//read about prefix and postfix operator in JS and also about operator precedence in JS
+
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
