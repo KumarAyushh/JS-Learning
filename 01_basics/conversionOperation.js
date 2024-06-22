a3db215b353a67add22b1066b043489ff4d35b8b
Kumar Ayush <108970673+KumarAyushh@users.noreply.github.com>
2024-06-22 08:50:24 +0000

    Added a new file having different conversions

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "33abc"
+// console.log(typeof score)
+// console.log(typeof (score));
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+//"33" => 33
+// "33abc" => NaN
+//true/false => 1/0
+//null => 0
+
+let isLogggedIn = 1
+let booleanIsLoggedIn = Boolean(isLogggedIn)
+//console.log(booleanIsLoggedIn);
+//1 => true; 0 => false
+//"" => false
+//"Ayush" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
